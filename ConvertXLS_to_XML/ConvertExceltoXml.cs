aac6a93c86b1e748614764b0fdfa95386aa637b2
muhammad <khabibulloevms@gmail.com>
2022-06-15 08:42:22 +0300

    First commit

---
+++ ConvertXLS_to_XML/ConvertExceltoXml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Bytescout.Spreadsheet;
+
+namespace ConvertXLS_to_XML
+{
+    class ConvertExceltoXml
+    {
+        static void Main(string[] args)
+        {
+            Spreadsheet document = new Spreadsheet();
+            document.LoadFromFile("D:\\Viggggi.xlsx");
+
+            // delete output file if exists already
+            if (File.Exists("Viggggi.xml"))
+            {
+                File.Delete("Viggggi.xml");
+            }
+
+            document.Workbook.Worksheets[0].SaveAsXML("Viggggi.xml");
+            document.Close();
+        }
+    }
+}
